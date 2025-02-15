4f221b624de45fb09cd364566b3c37a813e7881b
Insha0113 <inshanouri@gmail.com>
2025-02-15 17:13:59 +0530

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Sign-Language-Translator-Frontend/", // 👈 Add this line
+});
